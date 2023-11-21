94c706d3441fd574da4bcb271a0a6b373d94e53b
Isha Patro <isha.patro.215@gmail.com>
2023-11-21 15:24:45 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+
+export default function Tags() {
+  const [autoCompleteValue, setAutoCompleteValue] = useState(["foo", "bar"]);
+
+  return (
+    <div>
+      <Autocomplete
+        multiple
+        freeSolo
+        id="tags-outlined"
+        options={["foo", "bar"]}
+        defaultValue={["foo", "bar"]}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="filterSelectedOptions"
+            placeholder="Favorites"
+          />
+        )}
+      />
+    </div>
+  );
+}
